f0522e547dcdd416016dfbaa74d9d6673872834d
Denis <131021042+rexxar655@users.noreply.github.com>
2023-09-27 12:58:25 +0300

    Delete lesson 95 Wake bake directory

--- lesson 95 Wake bake/js/main.js
+++
@@ -1,6 +0,0 @@
-const burger = document.querySelector('.burger__icon')
-const body = document.querySelector('.body')
-
-burger.addEventListener('click', () => {
-    body.classList.togle('body--opened-menu')
-})
